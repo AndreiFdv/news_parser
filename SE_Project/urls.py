85b3d8bbed5bc83b6f5e9bdc15dfed34e4418062
AndreiFdv <andrei.fedotov@unilodz.eu>
2021-03-13 23:35:16 +0100

    Revert "Add template to news"

--- SE_Project/urls.py
+++ SE_Project/urls.py
@@ -16,16 +16,16 @@ Including another URLconf
 import debug_toolbar
 
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('News.urls')),
     path('admin/', admin.site.urls),
+
 ]
 
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns += path('__debug__/', include(debug_toolbar.urls)),
+    urlpatterns = [path('__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
