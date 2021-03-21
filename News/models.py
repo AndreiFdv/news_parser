7d096ad7e460731214d674b77b4ad802b220dbab
proger-xyz <khanbayov.tamerlan@gmail.com>
2021-03-21 06:29:47 +0100

    2.added article model 3.made migration

--- News/models.py
+++ News/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    CATEGORY = (
+        ('politics', 'politics'),
+        ('science', 'science'),
+        ('tech', 'tech'),
+    )
+    title = models.CharField(max_length=60)
+    date = models.DateField(auto_now_add=True)
+    article_category = models.CharField(max_length=10,choices=CATEGORY)
+    content=models.TextField()
+    source_link=models.TextField(max_length=2000)
