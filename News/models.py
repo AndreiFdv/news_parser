5fac192c94f7d554255c80071016467337f4d2ae
Andrei <timoha235@yandex.ru>
2021-04-08 15:42:44 +0200

    Add img to news

--- News/models.py
+++ News/models.py
@@ -5,9 +5,10 @@ class Article(models.Model):
     title = models.CharField(max_length=60)
     date = models.DateTimeField(blank=True)
     author = models.CharField(max_length=500, blank=True)
-    content = models.TextField(blank=True)
-    short_text = models.TextField(blank=True)
+    content = models.TextField(blank=True, null=True)
+    short_text = models.TextField(blank=True, null=True)
     source_link = models.CharField(max_length=2000, blank=True)
+    img = models.CharField(max_length=2000, blank=True, null=True)
 
     def __str__(self):
         return self.title
