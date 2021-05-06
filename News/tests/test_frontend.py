ea5e7b50f7ed4e5ea44f9febf361efd85ddb7ca0
Dev-Sherlock <khanbayov.tamerlan@gmail.com>
2021-05-06 11:24:44 +0200

    added more tests.

--- News/tests/test_frontend.py
+++ News/tests/test_frontend.py
@@ -6,22 +6,33 @@ from selenium import webdriver
 # Create your tests here.
 class Main(LiveServerTestCase):
     def setUp(self):
-        self.browser = webdriver.Chrome('/home/x/PycharmProjects/SE_Project/News/tests/chromedriver.exe')
+        self.browser = webdriver.Chrome()
 
     def tearDown(self):
         self.browser.close()
 
-    def test_this(self):
+    def test_title(self):
         self.browser.get(self.live_server_url)
         title = self.browser.title
-        logo = self.browser.find_element_by_class_name('navbar-brand')
-        navbar_item = self.browser.find_element_by_class_name('nav-link active')
         self.assertEquals(title, 'DDD news')
+
+    def test_logo(self):
+        self.browser.get(self.live_server_url)
+        logo = self.browser.find_element_by_class_name('navbar-brand')
         self.assertEquals(logo, 'DDD news')
+
+    def test_navbar_item(self):
+        self.browser.get(self.live_server_url)
+        navbar_item = self.browser.find_element_by_class_name('nav-link active')
         self.assertEquals(navbar_item, 'Home')
 
+    def tes_links(self):
+
+        self.browser.get(self.live_server_url)
         all_links = []
         for a in self.browser.find_elements_by_xpath('.//a'):
-            print(a.get_attribute('href'))
             all_links.append(a.get_attribute('href'))
-        time.sleep(10)
+
+        for i in range(len(all_links)):
+          response = self.browser.get(all_links[i])
+          self.assertEquals(response.status_code, 200)
